2f68b10e85e9d2c555504b19d926c8361a8da96e
andresito05 <guarnizoandres004@gmail.com>
2024-10-29 11:23:52 -0500

    config base de datos y entidades

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Product } from "./entities/Product";
+
+export const AppDataSource = new DataSource({
+    type: "sqlite", // tipo de base de datos
+    database: "database.sqlite", //nombre de base de datos 
+    synchronize: true, // sincronizacion activa
+    logging: false, //sin incio de sesion en la base
+    entities: [Product],  // se agrega las entidades creadas 
+    migrations:[], 
+    subscribers:[],
+    
+});
